3cd2f997d940e8ddb963f108bc54bc38bb058faf
Taylor Blocker <taylorblocker@gmail.com>
2019-11-10 11:14:52 -0600

    updated circles files with code put together in class by teacher and classmates

--- circles/App.js
+++ circles/App.js
@@ -1,9 +1,26 @@
 // Render the circles using React!
 const App = (props) => {
     let { circles } = props;
+    console.log('App component props:', props)
+
+    const AllCircles = props.circles.map(Circle)
+
     return (
-        <span>circles</span>
+        <span>{AllCircles}</span>
     )
 }
 
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
+const Circle = (props) => {
+    console.log("Circle component props:", props)
+
+    const style = {
+        background: props.color, 
+        borderRadius: '100%',
+        width: (props.radius * 2) + 'px', 
+        height: (props.radius * 2) + 'px'
+    }
+
+    return (
+      <div style={style}></div>
+    )
+}
