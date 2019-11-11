c88487ad5f219c1272d20b9cefee59658cb37b79
Taylor Blocker <taylorblocker@gmail.com>
2019-11-10 20:25:30 -0600

    completed rectangle exercises.

--- rectangle/App.js
+++ rectangle/App.js
@@ -1,7 +1,17 @@
 // Render the rectangle using React!
 const App = (props) => {
     let { rectangle } = props;
+
+    let style = {
+        height: rectangle.height,
+        width: rectangle.width,
+        backgroundColor: rectangle.color
+    }
+
+    console.log(rectangle)
     return (
-        <span>rectangle</span>
+        <span><div style={style}></div></span>
     )
 }
+
+
