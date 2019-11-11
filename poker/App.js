2501914cf0c90ee9efaa3155b0eb11d8548ce385
Taylor Blocker <taylorblocker@gmail.com>
2019-11-10 19:46:11 -0600

    completed cards exercise. Need to ask about rendering components of img tag outside first THEN inserting it into the return. Not sure why i can't just use props.value inside the return to construct.

--- poker/App.js
+++ poker/App.js
@@ -1,11 +1,21 @@
 // Render the cards using React!
 const App = (props) => {
     let { cards } = props;
+
+    const allCards = cards.map(Card)
+
     return (
-        <span>cards</span>
+        <span>{allCards}</span>
     )
 }
 
 // CHALLENGE: Write a separate Card component for use in the App component
 // TIP: Use props to pass the suit and number to each Card component
-const Card = null;  
+const Card = (props) => {
+
+    let card = "cards/" + props.value + props.suit + ".png"
+
+    return (
+        <img width='150' src={card} />
+    ) 
+} 
